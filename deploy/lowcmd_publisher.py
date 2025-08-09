b329e79780a725ad7113a5c81d7cf0bb8413ed33
Charlie Cheng <ctcheng@berkeley.edu>
2025-08-08 19:34:35 -0700

    publisher use built in crc unit

--- deploy/lowcmd_publisher.py
+++ deploy/lowcmd_publisher.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String  # Change according to your message type
 import time
-from legged_gym.teleop.crc import CRC
+from teleop.crc import CRC
 
 import numpy as np
 from unitree_hg.msg import (
@@ -18,21 +18,22 @@ crc = CRC()
 
 dt = (1 / HZ) - 8e-6
 
+
 class MessageRelayNode(Node):
     def __init__(self):
-        super().__init__('message_relay_node')
-        
+        super().__init__("message_relay_node")
+
         # Create a subscriber to listen to an input topic (such as 'input_topic')
         self.subscription = self.create_subscription(
             LowCmd,  # Replace with your message type
-            'lowcmd_buffer',  # Replace with your input topic name
+            "lowcmd_buffer",  # Replace with your input topic name
             self.listener_callback,
-            1
+            1,
         )
-        
+
         # Create a publisher to publish messages to the 'lowcmd' topic
-        self.publisher = self.create_publisher(LowCmd, 'lowcmd', 1)
-        
+        self.publisher = self.create_publisher(LowCmd, "lowcmd", 1)
+
         # Initialize a variable to store received messages
         self.last_msg = None
         self.last_last_msg = None
@@ -44,10 +45,10 @@ class MessageRelayNode(Node):
         self.new_msg.mode_machine = 5
         self.motor_cmd = []
         for id in range(29):
-            cmd=MotorCmd(q=0.0, dq=0.0, tau=0.0, kp=0.0, kd=0.0, mode=1, reserve=0)
+            cmd = MotorCmd(q=0.0, dq=0.0, tau=0.0, kp=0.0, kd=0.0, mode=1, reserve=0)
             self.motor_cmd.append(cmd)
         for id in range(29, 35):
-            cmd=MotorCmd(q=0.0, dq=0.0, tau=0.0, kp=0.0, kd=0.0, mode=0, reserve=0)
+            cmd = MotorCmd(q=0.0, dq=0.0, tau=0.0, kp=0.0, kd=0.0, mode=0, reserve=0)
             self.motor_cmd.append(cmd)
         self.new_msg.motor_cmd = self.motor_cmd.copy()
 
@@ -64,7 +65,6 @@ class MessageRelayNode(Node):
             self.last_last_msg.motor_cmd = self.last_msg.motor_cmd.copy()
             self.last_msg.motor_cmd = msg.motor_cmd.copy()
 
-    
     def set_motor_position(
         self,
     ):
@@ -91,32 +91,36 @@ class MessageRelayNode(Node):
             #     print(self.new_msg.motor_cmd[i].q)
             # print(self.new_msg.motor_cmd[18].q)
 
+
 def main(args=None):
     rclpy.init(args=args)
-    
+
     # Create node instance
     node = MessageRelayNode()
-    
-    print('Initialized')
+
+    print("Initialized")
     try:
         loop_start_time = time.monotonic()
         while rclpy.ok():
             # Process subscription callback functions
             rclpy.spin_once(node, timeout_sec=0)
-            
+
             # Forward message to lowcmd topic
             node.relay_message()
-            
-            while dt-time.monotonic()+loop_start_time>0:  #0.012473  0.019963 # Create 1000Hz publishing frequency
+
+            while (
+                dt - time.monotonic() + loop_start_time > 0
+            ):  # 0.012473  0.019963 # Create 1000Hz publishing frequency
                 pass
             loop_start_time = time.monotonic()
-    
+
     except KeyboardInterrupt:
         pass
-    
+
     # Close node
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
